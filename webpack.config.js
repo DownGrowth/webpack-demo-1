e711bba61bfb841bcd4129470290f8deaec9e3df
BK <1209157041@qq.com>
2022-07-18 21:42:47 +0800

    111

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
-var path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+
+const base = require('./webpack.config.base.js')
+
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js'
+    ...base, //把base的属性继承过来
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "./dist"
+    },
+    module: {
+        rules: [
+            ...base.module.rules,
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"] //开发模式，不新生成css文件，用js写在html里
+            }
+        ]
     }
 };
