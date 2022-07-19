78e853b4ddd137199c92ddc9f214a65e485df11c
BK <1209157041@qq.com>
2022-07-19 18:49:21 +0800

    上传

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -15,30 +15,34 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ["file-loader"]
+                test:/\.(png|svg|jpg|gif)$/,
+                use:["file-loader"]
             },
             {
                 test: /\.styl$/,
-                loader: ["style-loader", "css-loader", "stylus-loader"]
+                loader:['style-loader', 'css-loader','stylus-loader']
             },
             {
                 test: /\.less$/,
-                loader: ["style-loader", "css-loader", "less-loader"]
+                loader: ['style-loader', 'css-loader', 'less-loader'],
             },
+
             {
                 test: /\.scss$/i,
                 use: [
-                    "style-loader",
-                    "css-loader",
+                    // 将 JS 字符串生成为 style 节点
+                    'style-loader',
+                    // 将 CSS 转化成 CommonJS 模块
+                    'css-loader',
+                    // 将 Sass 编译成 CSS
                     {
                         loader: "sass-loader",
                         options: {
-                            implementation: require("dart-sass")
+                            implementation: require('dart-sass')
                         }
-                    }
-                ]
-            }
-        ]
-    }
+                    },
+                ],
+            },
+        ],
+    },
 };
