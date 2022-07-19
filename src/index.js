427f5e3453929acdf11dae2503a4ae6b62c5be8c
BK <1209157041@qq.com>
2022-07-19 19:11:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import x from './x.js'
 import png from './assets/1.png'
 console.log(x)
 
-const div=document.getElementById('app')
-div.innerHTML=`<img src="${png}">`
+// const div=document.getElementById('app')
+// div.innerHTML=`<img src="${png}">`
 
 
 const button =document.createElement('button')
